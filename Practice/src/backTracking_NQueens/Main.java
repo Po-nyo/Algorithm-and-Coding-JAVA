3ae61d147b1a10422d25f41267011c40f7b07f37
wkj1103 <wkj1103@naver.com>
2020-04-19 19:26:51 +0900

    N Queens - back tracking

---
+++ Practice/src/backTracking_NQueens/Main.java
@@ -0,0 +1,30 @@
+package backTracking_NQueens;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author wkj11
+ * N Queens
+ * 0이 아닌 정수 n이 주어졌을 때,
+ * n x n 크기의 체스판에 n개의 퀸을
+ * 서로 공격할 수 없도록 배치하는
+ * 모든 방법을 찾아내어 출력한다.
+ * (backtracking으로 구현)
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+		int n = sc.nextInt();
+		
+		N_Queens n_queens = new N_Queens(n);
+		n_queens.run();
+		sc.close();
+
+	}
+
+}
---
+++ Practice/src/backTracking_NQueens/N_Queens.java
@@ -0,0 +1,66 @@
+package backTracking_NQueens;
+
+public class N_Queens {
+	
+	int n;
+	int cnt;
+	
+	public N_Queens(int n) {
+		this.n = n;
+		this.cnt = 0;
+	}
+	
+	public void run() {
+		int[] queens = new int[n]; // index를 행, 원소를 열로 생각
+		
+		backtrack(queens, 0);
+		System.out.println("솔루션의 갯수 : "+cnt);
+	}
+	
+	private void backtrack(int[] queens, int row) {
+		if(row == n) {
+			printSolution(queens);
+			cnt++;
+		}
+		else {
+			for(int i=0; i<queens.length; i++) {
+				if(isPromising(queens, row, i)) {
+					queens[row] = i;
+					backtrack(queens, row+1);
+				}
+			}
+		}
+	}
+	
+	private boolean isPromising(int[] queens, int row, int i) {
+		
+		for(int j=0; j<row; j++) {
+			if(queens[j] == i ||
+			   queens[j]+(row-j) == i ||
+			   queens[j]-(row-j) == i) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void printSolution(int[] queens) {
+		
+		for(int i=0; i<queens.length; i++) {
+			for(int j=0; j<queens.length; j++) {
+				if(queens[j] == i)
+					System.out.print(" Q ");
+				else
+					System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public void setN(int n) {
+		this.n = n;
+		this.cnt = 0;
+	}
+}
