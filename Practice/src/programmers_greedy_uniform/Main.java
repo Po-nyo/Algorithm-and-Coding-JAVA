8f02a59792b1c9b781ed9ff620372cd23b42d1aa
WooKyungjin <wkj1103@naver.com>
2020-05-09 21:59:31 +0900

    .

--- Practice/src/programmers_greedy_uniform/Main.java
+++ Practice/src/programmers_greedy_uniform/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=30;
+		int n = 30;
 		int[] lost = {2, 3, 5, 6, 7, 8, 10, 11};
 		int[] reserve = {3, 4, 5, 7, 8, 9};
 
