6e7393db830058751af13d20074b99649471cfae
WooKyungjin <wkj1103@naver.com>
2020-09-24 03:14:59 +0900

    baekjoon 2839

--- Practice/src/baekjoon_2839/Main.java
+++ Practice/src/baekjoon_2839/Main.java
@@ -1,10 +1,31 @@
 package baekjoon_2839;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int count = 0;
+		
+		while(n >= 0) {
+			if(n % 5 == 0) {
+				count += n/5;
+				break;
+			}
+			else {
+				n -= 3;
+				count++;
+			}
+		}
+		
+		if(n < 0)
+			System.out.println(-1);
+		else
+			System.out.println(count);
+		
+		scan.close();
 	}
-
 }
